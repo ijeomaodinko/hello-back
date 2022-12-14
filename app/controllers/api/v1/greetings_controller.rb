031f3511524b32cd499192101569ff876858b33a
ijeomaodinko <ijeomaodinko@gmail.com>
2022-12-14 14:32:32 -0500

    Add files for controller

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,11 +1,11 @@
-class Api::GreetingController < ApplicationController
-        def index
-          @greeting = Greeting.random_greeting
-  
-          respond_to do |format|
-            format.html
-            format.json { render json: @greeting }
-          end
-        end
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        @greetings = Greeting.order(Arel.sql('RANDOM()')).first
+
+        render json: @greetings
+      end
+    end
+  end
 end
- 
